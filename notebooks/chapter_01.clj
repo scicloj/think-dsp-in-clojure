07b94c25ceaaa579018ab15a1d812bb7c26618eb
dotinspace <daniel@dotinspace.net>
2024-10-16 17:39:21 +0200

    Add example of writing a signal to file
    Made use of kind/hidden to suppress return value, a sizeable
    double array, which is of no use rendering, and also takes a lot of
    time to render.

--- notebooks/chapter_01.clj
+++ notebooks/chapter_01.clj
@@ -7,7 +7,8 @@
             [fastmath.signal :as signal]
             [scicloj.hanamicloth.v1.plotlycloth
              :as ploclo]
-            [scicloj.kindly.v4.api :as kindly]))
+            [scicloj.kindly.v4.api :as kindly]
+            [scicloj.kindly.v4.kind :as kind]))
 
 
 (defn plot-signal [signal time-range]
@@ -17,7 +18,7 @@
       (ploclo/layer-line {:=x :time
                           :=y :amplitude})))
 
-;; # Signals
+;; # Waveforms
 ;; ## Generate and plot a sinusoidal waveform
 (plot-signal (signal/oscillator :sin 1.0 1.0 0.0)
              (range 0 1 0.001))
@@ -35,8 +36,15 @@
   (-> (signal/oscillators-sum sig1 sig2)
       (plot-signal (range 0 3 0.01))))
 
-;; # Waves
-
+;; # Signals
+;; ## Generate a signal from a waveform and save to disk
+;; - At sample rate 44.1k, period of 1 second
+(kind/hidden (let [sig1 (signal/oscillator :sin 1 0.25 0)
+                   sig2 (signal/oscillator :sin 2 0.75 0)]
+               (-> (signal/oscillators-sum sig1 sig2)
+                   (signal/oscillator->signal 44100 1)
+                   (signal/save-signal "output/ch01/signal-01.raw"))))
+;; => #object["[D" 0x11c5b926 "[D@11c5b926"]
 
 
 
